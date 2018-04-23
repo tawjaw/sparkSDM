66dde7aba66d372a5c657aab7ad33ecc17138e44
tawjaw <tawjaw@gmail.com>
2018-04-22 22:03:53 -0400

    initial commit

---
+++ SDM.py
@@ -0,0 +1,232 @@
+#Author: Tawfiq Jawhar
+import pyspark
+import pyspark.mllib.random as random
+from pyspark.mllib.linalg import *
+from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
+import pyspark.sql as sql
+import numpy as np
+from pyspark.sql.types import FloatType
+from pyspark.sql.functions import udf, lit, col
+
+import h5py
+
+class SDM(object):
+    sc = None
+    class Settings:
+        def __init__(self,min,max,dim, numHL, radius):
+            """returns an instance of Settings used to build SDM.
+            Paramaters:
+                min: minimum range value of Integer SDM
+                max: maximum range value of Integer SDM
+                dim: number of dimensions of the memory
+                numHL: number of hard locations allocated in the memory
+            """
+            for parm in [min,max,dim,numHL]:
+                if not isinstance(parm, int):
+                    raise TypeError("min,max,dim and numHL must be integers.")
+            if not (isinstance(radius,float) or isinstance(radius,int)):
+                raise TypeError("radius must be int or float.")
+            if min >= max: raise AssertionError("minimum range can not be greater than the maximum range.")
+            self.min = min
+            self.max = max
+            self.dim = dim
+            self.numHL = numHL
+            self.radius = radius
+    
+    def __init__(self, sc,min,max,dim,numHL,radius,mode='nofile'):
+        """returns an instance of SDM.
+            Paramaters:
+                min: minimum range value of Integer SDM
+                max: maximum range value of Integer SDM
+                dim: number of dimensions of the memory
+                numHL: number of hard locations allocated in the memory
+                sc: SparkContext instance
+                mode:
+                    'nofile' (default) stores SDM's counters in numpy arrays
+                    'parquet-hdf5' stores SDM's hard location addresses in parquet file and counters in hdf5 file
+                    'hdf5' stores counters in hdf5 file and stores the random seed to generate the addresses RDD without storing the addresses in a file
+        """
+        SDM.sc = sc
+        self.settings = SDM.Settings(min,max,dim,numHL,radius)
+        if mode not in ['nofile','parquet-hdf5','hdf5']:
+            raise AssertionError("mode "+mode+" not defined")
+        self.mode = mode
+        self.addresses = None
+
+        
+    def __init__(self,sc,settings,mode='nofile'):
+        """returns an instance of SDM.
+            Paramaters:
+                settings: an instance of SDM.Settings
+                sc: SparkContext instance
+                mode:
+                    'nofile' (default) stores SDM's counters in numpy arrays
+                    'parquet-hdf5' stores SDM's hard location addresses in parquet file and counters in hdf5 file
+                    'hdf5' stores counters in hdf5 file and stores the random seed to generate the addresses RDD without storing the addresses in a file
+        """
+        SDM.sc = sc
+        if not isinstance(settings,SDM.Settings):
+            raise TypeError("settings is not instance of Settings.")
+        self.settings = settings
+        
+        if mode not in ['nofile','parquet-hdf5','hdf5']:
+            raise AssertionError("mode '"+mode+"' not defined")
+        self.mode = mode
+        self.addresses = None
+        
+    @staticmethod
+    def create_addresses_DF(settings, random_seed = None):
+        spark = SparkSession.builder.appName("sdm").config(conf=SparkConf()).getOrCreate()
+        HardLocations = random.RandomRDDs.uniformVectorRDD(SDM.sc, settings.numHL, settings.dim, seed=random_seed)\
+            .map(lambda x: (x*(settings.max-settings.min) + settings.min).astype(int))\
+            .zipWithIndex()\
+            .map(lambda row: (row[1], DenseVector(row[0])))\
+            .toDF(["id", "address"])
+        return HardLocations
+    
+
+    
+    @staticmethod
+    def activated_hard_locations(df, word, radius, max):
+        """returns a list of the index of activated hard locations using modular euclidean distance."""
+        def euclidean_distance(arr1,arr2):
+            """Returns modular euclidean distance"""
+            sum = 0
+            for i in range(len(arr1)):
+                temp1 = np.abs(arr1[i]-arr2[i])
+                temp2 = max - temp1
+                tempDist = np.square(temp1) if temp1 < temp2 else np.square(temp2)
+                sum = sum + tempDist
+            return np.sqrt(sum)
+        
+        spark = SparkSession.builder.appName("sdm").config(conf=SparkConf()).getOrCreate()
+        distance_udf = udf(lambda x: float(euclidean_distance(x, word)), FloatType())
+        df = df.withColumn('distances', distance_udf(col('address')))
+        return df.filter(df.distances < lit(radius)).select("id").rdd.map(lambda x: x[0]).collect()
+    
+    
+    @staticmethod
+    def write_addresses(df, file, mode):
+        spark = SparkSession.builder.appName("sdm").config(conf=SparkConf()).getOrCreate()
+        df.write.parquet(file, mode= mode)
+
+    def activated_HL(self,word, radius = None):
+        if radius == None: radius = self.settings.radius
+        activated = SDM.activated_hard_locations(self.addresses, word, radius, self.settings.max)
+        return activated
+
+    def create(self,file=None, overwrite = False):
+        """Creates an empty SDM memory.
+        Parameters:
+            file: (default = None) if the mode of SDM is 'parquet-hdf5' or 'hdf5' the file is the directory where the memory will be created and stored.
+            overwrite: (default = False) If overwrite is True then the memory will be overwrite any previous memory created in file. This parameter is applied only for file-based mode. 
+        """
+
+        if self.mode == 'nofile':
+            self.addresses = SDM.create_addresses_DF(self.settings)
+            self.counters = np.zeros((self.settings.numHL,(self.settings.max-self.settings.min+1),self.settings.dim))
+        elif self.mode == 'parquet-hdf5':
+            if not isinstance(file,str):
+                raise TypeError("Wrong type 'file'.")
+            
+            self.file = file
+            
+            self.addresses = SDM.create_addresses_DF(self.settings)
+            SDM.write_addresses(self.addresses, file+"addresses.parquet", 'overwrite' if overwrite else 'error')
+            countersF5 = h5py.File(file+"counters.hdf5", 'w')
+            countersF5.attrs['min'] = self.settings.min
+            countersF5.attrs['max'] = self.settings.max
+
+            countersF5.attrs['numHL'] = self.settings.numHL
+            countersF5.attrs['dim'] = self.settings.dim
+            countersF5.attrs['radius'] = self.settings.radius
+            countersF5.attrs['mode'] = self.mode
+            ## counters for one hard location
+            counter = np.zeros((self.settings.max-self.settings.min+1,self.settings.dim))
+            #for each HL create a dataset with zeroes initialized
+            for i in range(self.settings.numHL):
+                countersF5.create_dataset(str(i),data=counter)
+            countersF5.close()
+
+        elif self.mode == 'hdf5':
+            if not isinstance(file,str):
+                raise TypeError("Wrong type 'file'.")
+            random_seed = np.random.randint(0,100000)
+            self.file = file
+            countersF5 = h5py.File(file+"counters.hdf5", 'w')
+            countersF5.attrs['seed'] = random_seed
+            countersF5.attrs['min'] = self.settings.min
+            countersF5.attrs['max'] = self.settings.max
+            countersF5.attrs['numHL'] = self.settings.numHL
+            countersF5.attrs['dim'] = self.settings.dim
+            countersF5.attrs['radius'] = self.settings.radius
+            countersF5.attrs['mode'] = self.mode
+
+            ## counters for one hard location
+            counter = np.zeros((self.settings.max-self.settings.min+1,self.settings.dim))
+            #for each HL create a dataset with zeroes initialized
+            for i in range(self.settings.numHL):
+                countersF5.create_dataset(str(i),data=counter)
+            countersF5.close()
+
+
+                        
+    @staticmethod
+    def open(sc,file):
+        SDM.sc = sc
+        #read hdf5 metadata and get the info of SDM then load the addresses to DF 
+        spark = SparkSession.builder.appName("sdm").config(conf=SparkConf()).getOrCreate()
+
+        F5 = h5py.File(file+"counters.hdf5", 'r')
+        settings = SDM.Settings(int(F5.attrs['min']),int(F5.attrs['max']),int(F5.attrs['dim']),\
+                               int(F5.attrs['numHL']),int(F5.attrs['radius']))
+        mode = F5.attrs['mode']
+        
+        sdm = SDM(sc,settings, mode = mode)
+        random_seed = None
+        sdm.file = file
+        if mode == 'parquet-hdf5':
+            sdm.addresses = spark.read.parquet(file+"addresses.parquet")
+
+        elif mode == 'hdf5': 
+            random_seed = int(F5.attrs['seed'])
+            sdm.addresses = SDM.create_addresses_DF(settings,random_seed=random_seed)
+        F5.close()
+        return sdm
+
+    
+    def write(self, word):
+        assert len(word) == self.settings.dim
+        activated = SDM.activated_hard_locations(self.addresses, word, self.settings.radius,self.settings.max)
+        if self.mode == 'nofile':
+            for index in activated:
+                for i in range(len(word)):
+                    self.counters[index][word[i]][i]=self.counters[index][word[i]][i]+1
+        
+        elif self.mode == 'parquet-hdf5' or self.mode == 'hdf5':
+            countersF5 = h5py.File(self.file+"counters.hdf5", 'r+')
+            for index in activated:
+                dataset = countersF5[str(index)]
+                counter = dataset.value
+                for i in range(len(word)):
+                    counter[word[i]][i]=counter[word[i]][i]+1
+                dataset[...] = counter
+            countersF5.close()
+
+                    
+    def read(self, word):
+        assert len(word) == self.settings.dim
+        activated = SDM.activated_hard_locations(self.addresses, word, self.settings.radius,self.settings.max)
+        if self.mode == 'nofile':
+            activatedCounters = list()
+            for index in activated:
+                activatedCounters.append(self.counters[index])
+            return list(np.argmax(np.sum(activatedCounters,axis=0),axis=0))
+        if self.mode == 'parquet-hdf5' or self.mode == 'hdf5':
+            countersF5 = h5py.File(self.file+"counters.hdf5", 'r')
+            activatedCounters = list()
+            for index in activated:
+                activatedCounters.append(countersF5[str(index)].value)
+            countersF5.close()
+            return list(np.argmax(np.sum(activatedCounters,axis=0),axis=0))
---
+++ test.py
@@ -0,0 +1,63 @@
+from SDM import *
+import numpy as np
+import time
+
+import pyspark
+from pyspark import SparkConf, SparkContext
+
+sc = pyspark.SparkContext("local", "testsdm")
+
+min = 0
+max = 15
+dim = 500
+tests=[(1000,94,"SDM_1000"),(10000,92,"SDM_10000"),(100000,91,"SDM_100000")]
+
+#100 data words to write and read from memory
+testData = np.random.randint(min,max,(100,dim))
+
+
+"""
+create the 3 modes of SDM memory with the 3 numHL settings stated above.
+Write and read 100 data point randomly generated in testData.
+for 'nofile' mode create will be used to open the memory
+
+nofile  parquet-hdf5   hdf5
+[0,         0,         0] --> tests[0]
+[0,         0,         0] --> tests[1]
+[0,         0,         0] --> tests[2]
+
+"""
+testTimeCreate = [[0,0,0]]*3
+testTimeOpenWrite =[[0,0,0]]*3
+testTimeOpenRead = [[0,0,0]]*3
+
+##test creating memory
+for test in range(3):
+    settings = SDM.Settings(min,max,dim,tests[test][0],tests[test][1])
+    file = tests[test][2]
+    #no file memory
+    start = time.time()
+    sdm = SDM(sc,settings,mode='nofile')
+    sdm.create()
+    end = time.time()
+    testTimeCreate[test][0] =end - start
+    print("test: " + str(test)+" no file: Done")
+    del sdm
+    #parquet-hdf5 memory
+    start = time.time()
+    sdm = SDM(sc,settings,mode='parquet-hdf5')
+    sdm.create(file+'-parquet-hdf5\\')
+    end = time.time()
+    testTimeCreate[test][1] =end - start
+    print("test: " + str(test)+" parquet-hdf5: Done")
+    del sdm
+    #hdf5 memory
+    start = time.time()
+    sdm = SDM(sc,settings,mode='hdf5')
+    sdm.create(file+'-mmhdf5\\')
+    end = time.time()
+    testTimeCreate[test][2] =end - start
+    print("test: " + str(test)+" hdf5: Done")   
+    del sdm
+
+print(testTimeCreate)
